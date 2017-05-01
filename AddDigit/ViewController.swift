ce8a1405d9ac226476121d42b4b82d0de1acc3db
Wipro CDC-3 <wipro@Wipros-MacBook-Pro.local>
2017-05-01 09:51:44 -0700

    Initial Commit

---
+++ AddDigit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddDigit
+//
+//  Created by Wipro CDC-3 on 01/05/17.
+//  Copyright © 2017 Something. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
